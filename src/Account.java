19127b30cf1938c99dfd625345f8517987e37322
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-10-04 18:46:15 -0400

    initial setup for classes

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,38 @@
-public class Account {
+public abstract class Account implements Comparable <Account> {
+    protected Profile profileHolder;
+    protected double balance;
+
+
+    /**
+     * Contrustor with default values
+     * @param profileHolder
+     * @param balance
+     */
+    public Account(Profile profileHolder, double balance){
+        this.profileHolder = profileHolder;
+        this.balance = balance;
+    }
+
+    /**
+     *
+     */
+    public Profile getProfile(){
+        return this.profileHolder;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+
+    public abstract double monthlyInterest();
+    public abstract double monthlyFee();
+
+    @Override
+    public int compareTo(Account OtherAccount){
+        //implement logic here
+        return 0;
+    }
+
+
 }
--- src/Profile.java
+++ src/Profile.java
@@ -1,2 +1,33 @@
-public class Profile {
+public class Profile implements Comparable<Profile> {
+    private String name;
+    private String address;
+    private String dob;
+
+    public Profile(String name, String address, String dateOfBirth) {
+        this.name = name;
+        this.address = address;
+        this.dob = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDateOfBirth() {
+        return dob;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: " + name + "Address: " + address + "Date of birth: " + dob;
+    }
+
+    @Override
+    public int compareTo(Profile o) {
+        return 0;
+    }
 }
