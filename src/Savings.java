7870f0eeff3b5ac58d5bcc5a67308e27d623313a
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-10-04 18:49:38 -0400

    initial setup for savingsClass

--- src/Savings.java
+++ src/Savings.java
@@ -1,2 +1,34 @@
-public class Savings {
+public class Savings extends Account {
+    protected boolean isLoyal;
+
+    // Constants for interest rate and fee
+    private static final double DEFAULT_INTEREST_RATE = 0.02; // 2% interest rate
+    private static final double LOYALTY_INTEREST_RATE = 0.03; // 3% interest rate for loyal customers
+    private static final double MONTHLY_FEE = 2.00;
+
+    /**
+     * Contrustor with default values
+     *
+     * @param profileHolder
+     * @param balance
+     */
+    public Savings(Profile profileHolder, double balance) {
+        super(profileHolder, balance);
+    }
+
+    public boolean isLoyal() {
+        return isLoyal;
+    }
+
+    @Override
+    public double monthlyInterest() {
+        //implement logic
+        return 0.0;
+    }
+
+    @Override
+    public double monthlyFee() {
+        //implement logic
+        return 0.0;
+    }
 }
