54ae51c7b4c863dabe76983c4fc9e89a36cee2ea
MichaelMuz <20muzm01@gmail.com>
2023-10-12 21:28:39 -0400

    Additional progress

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class TransactionManager {
 
-    AccountDataBase accountDataBase;
+    AccountDatabase accountDatabase;
 
     private static final int COMMAND_TYPE_INDEX = 0;
     private static final int ACCOUNT_TYPE_INDEX = 1;
@@ -16,16 +16,12 @@ public class TransactionManager {
     private static final int LAST_NAME_INDEX = 3;
     private static final int DATE_INDEX = 4;
     private static final int MONEY_AMOUNT_INDEX = 5;
-//    private static final int MONTH_INDEX = 0;
-//
-//    private static final int DAY_INDEX = 1;
-//    private static final int YEAR_INDEX = 2;
-//    private static final int EXPECTED_NUMBER_OF_DATE_TOKENS = 0;
-//
-//    private static final int MIN_NUMBER_OF_DIGITS_IN_DAY_OR_MONTH = 0;
+    private static final int ADDITIONAL_INFO_INDEX = 6;
 
-    public TransactionManager(AccountDataBase accountDataBase){
-        this.accountDataBase = accountDataBase;
+    private static final int MAX_COMMAND_TOKENS = 7;
+
+    public TransactionManager(AccountDatabase AccountDatabase){
+        this.accountDatabase = AccountDatabase;
     }
 
 //    private static String parseDate(String dateString){
@@ -48,26 +44,75 @@ public class TransactionManager {
 //
 //    }
 
-    private static String parseCommand(String commandToken){
+    private static Account openProperAccount(String accountTypeString, Profile profileHolder, double balance, String additionalInfoString){
 
+        if(accountTypeString.equals("C")) {
+            return new Checking(profileHolder, balance);
+        }
+        else if(accountTypeString.equals("CC")) {
+            //needs campus
+            return new CollegeChecking(profileHolder, balance);
+        }
 
+        boolean loyalty;
+        if(additionalInfoString.equals("1")) {
+            loyalty = true;
+        }
+        else if(additionalInfoString.equals("0")) {
+            loyalty = false;
+        }
+        else{
+            System.out.print("loyalty is either 1 or 0");
+            return null;
+        }
+
+        if(accountTypeString.equals("S")) {
+            return new Savings(profileHolder, balance, loyalty);
+        }
+        else if(accountTypeString.equals("MM")) {
+            return new MoneyMarket(profileHolder, balance, loyalty);
+        }
+
+        return null;
+    }
+
+    private static Double parseMoneyAmount(String moneyAmountString){
+        boolean haveSeenDecimal = false;
+        for(int i = 0; i < moneyAmountString.length(); i++){
+            if(moneyAmountString.charAt(i) == '.'){
+                if(haveSeenDecimal){
+                    System.out.print("Cannot have 2 decimals in one String");
+                    return null;
+                }
+                haveSeenDecimal = true;
+            }
+            else if(!Character.isLetter(moneyAmountString.charAt(i))){
+                System.out.print("Non-numeric char detected");
+                return null;
+            }
+        }
+        return Double.parseDouble(moneyAmountString);
     }
 
     private void handleCommands(String userInput){
         String[] tokens = userInput.split("\\s+");
+        if(tokens.length > MAX_COMMAND_TOKENS){
+            System.out.print("Too many tokens");
+            return;
+        }
 
         //String commandType = TransactionManager.parseCommand(tokens[COMMAND_TYPE_INDEX]);
         String commandType = tokens[COMMAND_TYPE_INDEX];
 
         switch(commandType){
             case "P":
-                this.accountDataBase.printSorted();
+                this.accountDatabase.printSorted();
                 break;
             case "PI":
-                this.accountDataBase.printFeesAndInterests();
+                this.accountDatabase.printFeesAndInterests();
                 break;
             case "UB":
-                this.accountDataBase.printUpdatedBalances();
+                this.accountDatabase.printUpdatedBalances();
                 break;
             case "Q":
                 System.out.println("goodbye");
@@ -78,32 +123,46 @@ public class TransactionManager {
         String accountType = tokens[ACCOUNT_TYPE_INDEX];
         String firstName = tokens[FIRST_NAME_INDEX];
         String lastName = tokens[LAST_NAME_INDEX];
-
         Date dateOfBirth = Date.parseDate(tokens[DATE_INDEX]);
+        String additionalInfo = null;
+        if(tokens.length > ADDITIONAL_INFO_INDEX){
+            additionalInfo = tokens[ADDITIONAL_INFO_INDEX];
+        }
+        Double moneyAmount = TransactionManager.parseMoneyAmount(tokens[MONEY_AMOUNT_INDEX]);
+        if(moneyAmount == null){
+            return;
+        }
+        Profile profile = new Profile(firstName, lastName, dateOfBirth);
+
+        Account account = openProperAccount(accountType, profile, moneyAmount, additionalInfo);
+        if(account == null){
+            return;
+        }
 
         if(commandType.equals("O")){
-            this.accountDataBase.open();
+
+            this.accountDatabase.open(account);
         }
         //parse money amount
-        double moneyAmount = TransactionManager.parseMoneyAmount(tokens[MONEY_AMOUNT_INDEX]);
+
         switch(commandType){
             case "C":
-                this.accountDataBase.close();
+                this.accountDatabase.close(account);
                 break;
             case "D":
-                this.accountDataBase.deposit()
+                this.accountDatabase.deposit(account, moneyAmount);
                 break;
             case "W":
-                this.accountDataBase.withdraw();
+                this.accountDatabase.withdraw(account, moneyAmount);
                 break;
         }
     }
 
-    public void run(String [] args){
+    public void run(){
         Scanner in = new Scanner(System.in);
         while(in.hasNext()){
             String userInput = in.nextLine().trim();
-            if(userInput.length() == 0){
+            if(userInput.isEmpty()){
                 continue;
             }
             this.handleCommands(userInput);
