e01826bf07784b06c818152a6622a4479b3988fe
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-10-16 13:48:47 -0400

    updates unit tests

--- test/DateTest.java
+++ test/DateTest.java
@@ -4,17 +4,27 @@ import static org.junit.Assert.*;
 public class DateTest {
     @Test
     public void isValid() {
-
-        //Test valid dates
+        //Test valid date
         assertTrue(new Date(9, 12, 1900).isValid());
-        assertTrue(new Date(1, 1, 2022).isValid());
-        assertTrue(new Date(29, 2, 2024).isValid());
+    }
 
-        // Test invalid dates
+    @Test
+    public void inValidDay(){
         assertFalse(new Date(0, 1, 2022).isValid());  // Invalid day
-        assertFalse(new Date(1, 0, 2022).isValid());  // Invalid month
-        assertFalse(new Date(1, 1, 1899).isValid());  // Year before the minimum (1900)
-        assertFalse(new Date(1, 13, 2022).isValid());  // Invalid month
+    }
 
+    @Test
+    public void invalidMonth(){
+        assertFalse(new Date(1, 0, 2022).isValid());  // Invalid montn
+    }
+
+    @Test
+    public void notMinYear(){
+        assertFalse(new Date(1, 1, 1899).isValid());  // Year before the minimum (1900)
+    }
+
+    @Test
+    public void inValidMonth(){
+        assertFalse(new Date(1, 13, 2022).isValid());  // Invalid month
     }
 }
